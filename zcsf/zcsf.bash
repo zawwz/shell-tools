a9992d28771a43c91c42219d9cb2e4d3722e2f5a
zawz <zawz@hotmail.fr>
2020-05-03 17:11:27 +0200

    zcsf: fix bash completion on -

--- zcsf/zcsf.bash
+++ zcsf/zcsf.bash
@@ -9,7 +9,7 @@ _zcsf_completion()
 
   if [ "$COMP_CWORD" = "1" ] ; then # operations
     _compwords=$_cw1
-  elif [ "$COMP_CWORD" -gt "1" ] && echo "$_cw1_ls" | grep -qw "${COMP_WORDS[1]}" ; then # src files
+  elif [ "$COMP_CWORD" -gt "1" ] && echo "$_cw1_ls" | grep -qw -- "${COMP_WORDS[1]}"; then # src files
     _compwords=$(zcsf ls 2>/dev/null)
   elif [ "$COMP_CWORD" = "2" ] && [ "${COMP_WORDS[1]}" = "make" ] ; then # make type
     _compwords=$_make_types
